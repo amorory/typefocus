9471286aae4aad96937e9743124a5a03e3242b21
amorydoerr <amorydoerr@gmail.com>
2022-05-01 14:22:25 -0500

    Correct magnification

--- Control.cpp
+++ Control.cpp
@@ -4,43 +4,50 @@
 #include <oleacc.h>
 #include <windows.h>
 
-#include <iostream>
-
 Control* Control::control_ = nullptr;
 
 Control::Control(HWND parent) {
   control_ = this;
   hwnd_parent_ = parent;
-  current_zoom_ = 2.0f;
   lens_width_ = 300;
   lens_height_ = 100;
   magnifying_ = true;
-  hook_ = SetWinEventHook(
-      EVENT_OBJECT_LOCATIONCHANGE, EVENT_OBJECT_LOCATIONCHANGE, NULL,
-      (WINEVENTPROC)HookProcess, 0, 0, WINEVENT_OUTOFCONTEXT);
+  caret_position_.x = 0;
+  caret_position_.y = 0;
+  hook_ =
+      SetWinEventHook(EVENT_OBJECT_CREATE, EVENT_OBJECT_LOCATIONCHANGE, NULL,
+                      (WINEVENTPROC)HookProcess, 0, 0, WINEVENT_OUTOFCONTEXT);
   hwnd_ = CreateWindow(WC_MAGNIFIER, TEXT("MagnifierWindow"),
-                       WS_CHILD | MS_SHOWMAGNIFIEDCURSOR | WS_VISIBLE, 0, 0,
+                       WS_CHILD | MS_CLIPAROUNDCURSOR | WS_VISIBLE, 0, 0,
                        lens_width_, lens_height_, hwnd_parent_, nullptr,
                        GetModuleHandle(nullptr), nullptr);
+  SetZoom(2.0f);
   UINT_PTR timerId = SetTimer(hwnd_parent_, 0, 16, TimerProcess);
 }
 
 Control::~Control() { UnhookWinEvent(hook_); }
 
-void Control::SetZoom(float zoom_factor) { current_zoom_ = zoom_factor; }
+void Control::SetZoom(float zoom_factor) {
+  current_zoom_ = zoom_factor;
+
+  MAGTRANSFORM matrix;
+  memset(&matrix, 0, sizeof(matrix));
+  matrix.v[0][0] = current_zoom_;
+  matrix.v[1][1] = current_zoom_;
+  matrix.v[2][2] = 1.0f;
+
+  MagSetWindowTransform(hwnd_, &matrix);
+}
 
 void Control::Update() {
   if (magnifying_) {
     SetLayeredWindowAttributes(hwnd_parent_, 0, 255, LWA_ALPHA);
 
-    int border_width = GetSystemMetrics(SM_CXFIXEDFRAME);
-    int caption_height = GetSystemMetrics(SM_CYCAPTION);
-
     RECT source_rect;
     source_rect.left =
-        caret_position_.x - (((lens_width_ / 2)) / current_zoom_);
-    source_rect.top = caret_position_.y -
-                      (((lens_height_ / 2) + caption_height) / current_zoom_);
+        caret_position_.x - (int)((lens_width_ / 2) / current_zoom_);
+    source_rect.top =
+        caret_position_.y - (int)((lens_height_ / 2) / current_zoom_);
     source_rect.right = source_rect.left + (lens_width_ / current_zoom_);
     source_rect.bottom = source_rect.top + (lens_height_ / current_zoom_);
 
@@ -55,7 +62,7 @@ void Control::Update() {
     // Force the magnifier control to redraw itself.
     InvalidateRect(hwnd_, NULL, TRUE);
   } else {
-    SetLayeredWindowAttributes(control_->hwnd_parent_, 0, 0, LWA_ALPHA);
+    SetLayeredWindowAttributes(hwnd_parent_, 0, 0, LWA_ALPHA);
     InvalidateRect(hwnd_, NULL, TRUE);
   }
 }
@@ -69,13 +76,11 @@ void CALLBACK Control::HookProcess(HWINEVENTHOOK hEvent, DWORD event,
                                    HWND hwndMsg, LONG idObject, LONG idChild,
                                    DWORD idThread, DWORD dwmsEventTime) {
   if (idObject == OBJID_CARET) {
-    if (event == EVENT_OBJECT_SHOW) {
+    if (event == EVENT_OBJECT_CREATE || event == EVENT_OBJECT_SHOW) {
       control_->magnifying_ = true;
-    }
-    if (event == EVENT_OBJECT_HIDE) {
+    } else if (event == EVENT_OBJECT_DESTROY || event == EVENT_OBJECT_HIDE) {
       control_->magnifying_ = false;
-    }
-    if (event == EVENT_OBJECT_LOCATIONCHANGE) {
+    } else if (event == EVENT_OBJECT_LOCATIONCHANGE) {
       IAccessible* pAcc = nullptr;
       VARIANT varChild;
 
